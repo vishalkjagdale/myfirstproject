f9743c72b7dd64673a83ee3e9e385bb1a5f112bd
Vishal Jagdale <vishalkjagdale@gmail.com>
2025-11-18 15:05:41 +0530

    Add files via upload

---
+++ Datatypes.playground/Contents.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+//String is regular text
+let myFirstItem: String = "Hello, Kaleji!"
+
+// Reference previously created objects
+let myTitle = myFirstItem
+
+// Boolean (aka Bool) is true or false
+
+let mySecondItem:Bool = true
+let myThirdItem:Bool = false
+
+// Do not do
+//let myFourthItem: String = "true"
+
+// Swift is a type based language
+let myFifthItem: Bool = true
+//let mySixthitem:String = true
+let mySixthitem:String = "wow amazing"
+
+let mySeventhItem = true // we cant change once datatype is fixed
+// In above eg. its saved to Bool
+
+// Date
+let myFirstDate: Date = Date()
+
+// Number can be Int, Double, CGFloat & more
+// Int is a whole Integer
+let myFirstNumber = 1
+
+// Use Double for Math
+let mySecondNumber:Double = 1.0
+// Use CGFloat for UI
+let myThirdNumber:CGFloat = 1.1
+print(myFirstItem,mySecondItem,myThirdItem,myFifthItem,mySixthitem,mySeventhItem,)
+print(myFirstDate,myFirstNumber,mySecondNumber,myThirdNumber)
+
+
+
+
+
+
+
+
